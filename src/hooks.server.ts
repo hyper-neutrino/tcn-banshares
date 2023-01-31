8067a6ba88977471936fafe1a168e166e0d6ded7
hyper-neutrino <alex.yj.liao@gmail.com>
2023-01-31 02:19:37 -0500

    auto-start bot on load

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,8 @@
 import type { Handle } from "@sveltejs/kit";
 import { auth } from "./auth.js";
 
+import "./bot.js";
+
 export const handle: Handle = async ({ event, resolve }) => {
     console.log(
         `[${event.request.method}] ${event.url.href.substring(
